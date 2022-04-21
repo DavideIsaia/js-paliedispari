224569ac4466bc53b4d5a774230ef364d4e70298
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-21 15:26:34 +0200

    definite le 2 funzioni da usare

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -5,3 +5,25 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+
+// FUNCTIONS
+
+/**
+ * Descrizione: la funzione genera un numero da 1 a 5 e lo dà come output.
+ * @returns {any} numero da 1 a 5.
+ */
+function randomNumber1to5 () {
+
+    return randomGenerated;
+}
+
+/** 
+ * Descrizione: la funzione somma 2 numeri e ci dice se il risultato è pari o dispari.
+ * @param {number} num1 1° numero da sommare.
+ * @param {number} num2 2° numero da sommare.
+ * @returns {number} pari o dispari.
+ */
+function pariDispari (num1, num2) {
+
+    return oddOrEven;
+}
