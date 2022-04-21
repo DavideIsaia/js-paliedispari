53646703d74964256772c97af98cf46a5486f345
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-21 14:51:16 +0200

    implementata funzione e testata

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,36 @@
 // **Palindroma**
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+/**FUNCTIONS
+ * 
+ * Descrizione: la funzione riceve come input una parola e dà come output true se è palindroma e false se non lo è.
+ * @param {any} stringa parola o frase con spazi (funziona anche con cifre).
+ * @returns {any} true o false.
+ */
+function checkPalindroma (stringa) {
+
+    // definisco la lunghezza della stringa, dividendola per 2
+    let lunghezza = Math.floor(stringa.length / 2);
+
+    // variabile di default per scrivere un solo return
+    let palindroma = true;
+
+    // ciclo in cui controllo ogni elemento della stringa
+    for (let i = 0; i < lunghezza; i++) {
+
+        // se il primo elemento della stringa è diverso dall'ultimo allora non è palindroma ed avrò false,
+        // altrimenti ricomincio il ciclo e procedo confrontando il 2° elemento con il penultimo e così via
+        if (stringa[i] !== stringa[stringa.length - i - 1]) {
+            palindroma = false;
+        }
+
+    return palindroma;
+    }
+}
+
+// TEST
+let giusta = checkPalindroma("i topi non avevano nipoti");
+console.log(giusta);
+let sbagliata = checkPalindroma ("frase non palindroma");
+console.log(sbagliata);
